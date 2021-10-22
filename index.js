0eae931daf5241e3e0db447adb9d5b15d092cf1a
theFlexOne <edhavey@null.net>
2021-10-22 15:03:24 -0500

    server sends correct weather data back to browser

--- index.js
+++ index.js
@@ -6,35 +6,27 @@ app.listen(3000, (req, res) => console.log("Up at localhost:3000"));
 app.use(express.static('public'));
 app.use(express.json());
 
+
+
 app.post('/api', async (req, res) => {
   console.log("Request received");
   const {lat, lon} = req.body;
-  fetchWeather(lat, lon);
-
-
+  weatherData = await fetchWeather(lat, lon);
+  console.log(weatherData);
   res.json({
     status: "success",
-    lat,
-    lon
+    weatherData,
   });
 });
 
+
+
 const fetchWeather = async (lat, lon) => {
   console.log(lat, lon);
-  const url = "https://api.openweathermap.org/data/2.5/onecall?lat=90&lon=135&units=imperial&appid=37fc3c7b9cfbdc5c99764ee1486ef34d"
-
-  const weatherData = await ( await fetch(url)).json();
-  console.log(weatherData);
-
-  // const OPEN_WEATHER_API_ENDPOINT = "https://api.openweathermap.org/data/2.5/onecall?";
-  // const OPEN_WEATHER_API_KEY = "37fc3c7b9cfbdc5c99764ee1486ef34d";
-  // const units = "imperial";
-  // const url = `${OPEN_WEATHER_API_ENDPOINT}lat=${lat}&lon=${lon}&units=${units}&lang=en&appid=${OPEN_WEATHER_API_KEY}`;
-  // fetch(url)
-  //   .then(res => {
-  //     if (!res.ok) throw new Error(res.statusText);
-  //     return res.json();
-  //   })
-  //   .then(weatherData => weatherData)
-  //   .catch(console.error);
+  const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=imperial&appid=37fc3c7b9cfbdc5c99764ee1486ef34d`
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
+  return data;
 };
+
